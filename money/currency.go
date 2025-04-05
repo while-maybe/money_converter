dc34dabdcbfcb37f3c72db26add3d71ba9a07c38
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-04-05 17:00:44 +0100

    feat: add currency parser

--- money/currency.go
+++ money/currency.go
@@ -1,6 +1,37 @@
 package money
 
-// Currency defines the code of a currency
+import (
+	"regexp"
+	"strings"
+)
+
+// Currency defines the code of a currency and its decimal precision.
 type Currency struct {
-	code string
+	code      string
+	precision byte
+}
+
+// ErrInvalidCurrencyCode is returned when the currency to parse is not a standard 3-letter code
+const ErrInvalidCurrencyCode = Error("")
+
+// ParseCurrency returns the currency associated to a name and may return ErrInvalidCurrencyCode
+func ParseCurrency(code string) (Currency, error) {
+	code = strings.ToUpper(code)
+
+	validCodeChars := regexp.MustCompile(`^[A-Z]\{3}$`)
+
+	if len(code) != 3 || !validCodeChars.MatchString(code) {
+		return Currency{}, ErrInvalidCurrencyCode
+	}
+
+	switch code {
+	case "IRR":
+		return Currency{code: code, precision: 0}, nil
+	case "CNY", "VND":
+		return Currency{code: code, precision: 1}, nil
+	case "BHD", "IQD", "KWD", "LYD", "OMR", "TND":
+		return Currency{code: code, precision: 3}, nil
+	default:
+		return Currency{code: code, precision: 2}, nil
+	}
 }
