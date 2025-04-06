ba9d0625e1e260a8991f83ce489cb62c6f4af284
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-04-06 19:55:01 +0100

    feat: add NewAmount() constructor

--- money/amount.go
+++ money/amount.go
@@ -5,3 +5,20 @@ type Amount struct {
 	quantity Decimal
 	currency Currency
 }
+
+const (
+	// ErrTooPrecise is returned if the number is too precise for its currency.
+	ErrTooPrecise = Error("quantity is too precise")
+)
+
+// NewAmount returns an Amount of money.
+func NewAmount(quantity Decimal, currency Currency) (Amount, error) {
+	if quantity.precision > currency.precision {
+		// In order to avoid converting 0.00001 cent, let's exit now.
+		return Amount{}, ErrTooPrecise
+	}
+
+	quantity.precision = currency.precision
+
+	return Amount{quantity: quantity, currency: currency}, nil
+}
