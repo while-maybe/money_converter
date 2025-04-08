35ead2cbcca95fc0a95424993257500d6ebc0721
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-04-08 01:57:12 +0100

    refactor: read flags and args from CLI

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	from := flag.String("from", "", "source currency, required")
+	to := flag.String("to", "EUR", "target currency")
+	flag.Parse()
+
+	value := flag.Arg(0)
+	if value == "" {
+		_, _ = fmt.Fprintln(os.Stderr, "missing amount to convert")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	fmt.Println(*from, *to, value)
+}
