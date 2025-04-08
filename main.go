af09d37e52bc73fab9bb23adb73d9ba7ab854e38
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-04-08 04:12:55 +0100

    refactor: add parsing and logic to main()

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"moneyconverter/money"
 	"os"
 )
 
@@ -11,6 +12,17 @@ func main() {
 	to := flag.String("to", "EUR", "target currency")
 	flag.Parse()
 
+	fromCurrency, err := money.ParseCurrency(*from)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to parse source currency %q: %s.\n", *from, err.Error())
+		os.Exit(1)
+	}
+	toCurrency, err := money.ParseCurrency(*to)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to parse target currency %q: %s.\n", *to, err.Error())
+		os.Exit(1)
+	}
+
 	value := flag.Arg(0)
 	if value == "" {
 		_, _ = fmt.Fprintln(os.Stderr, "missing amount to convert")
@@ -18,5 +30,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(*from, *to, value)
+	quantity, err := money.ParseDecimal(value)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to parse value %q: %s.\n", value, err.Error())
+		os.Exit(1)
+	}
+
+	amount, err := money.NewAmount(quantity, fromCurrency)
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println("Amount:", amount, "; Currency", toCurrency)
 }
