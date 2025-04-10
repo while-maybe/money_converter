387efd6d9970d28449c12a2d425411012a695a31
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-04-10 01:21:23 +0100

    feat: add .exchangeRate() to convert a single rate
    Refactor .exchangeRates() to return map of float64 as per the data provided by the webAPI

--- ecbank/envelope.go
+++ ecbank/envelope.go
@@ -1,6 +1,7 @@
 package ecbank
 
 import (
+	"fmt"
 	"moneyconverter/money"
 )
 
@@ -9,24 +10,55 @@ type envelope struct {
 }
 
 type currencyRate struct {
-	Currency string             `xml:"currency,attr"`
-	Rate     money.ExchangeRate `xml:"rate,attr"`
+	Currency string  `xml:"currency,attr"`
+	Rate     float64 `xml:"rate,attr"`
 }
 
 const baseCurrencyCode = "EUR"
 
 // exchangeRates builds a map of all the supported exchange rates.
-func (e envelope) exchangeRates() map[string]money.ExchangeRate {
-	rates := make(map[string]money.ExchangeRate, len(e.Rates)+1)
+func (e envelope) exchangeRates() map[string]float64 {
+	rates := make(map[string]float64, len(e.Rates)+1)
 
 	for _, c := range e.Rates {
 		rates[c.Currency] = c.Rate
 	}
 
 	// represents EUR to EUR rate
-	one, _ := money.ParseDecimal("1")
-
-	rates[baseCurrencyCode] = money.ExchangeRate(one)
+	rates[baseCurrencyCode] = 1
 
 	return rates
+}
+
+// exchangeRate reads the change rate from the Envelope's contents.
+func (e envelope) exchangeRate(source, target string) (money.ExchangeRate, error) {
+	if source == target {
+		// no change rate for the same target and source
+		one, err := money.ParseDecimal("1")
+		if err != nil {
+			return money.ExchangeRate{}, fmt.Errorf("unable to create a rate of value 1: %w", err)
+		}
+		return money.ExchangeRate(one), nil
+	}
+
+	rates := e.exchangeRates()
+
+	sourceFactor, sourceFound := rates[source]
+	if !sourceFound {
+		return money.ExchangeRate{}, fmt.Errorf("failed to find the source currency %s", source)
+	}
+
+	targetFactor, targetFound := rates[target]
+	if !targetFound {
+		return money.ExchangeRate{}, fmt.Errorf("failed to find the target currency %s", target)
+	}
+
+	// use a precision of 10 digits after the decimal separator.
+	// This precision should be enough as most currencies only use 5 digits
+	rate, err := money.ParseDecimal(fmt.Sprintf("%.10f", targetFactor/sourceFactor))
+	if err != nil {
+		return money.ExchangeRate{}, fmt.Errorf("unable to parse exchange rate from %s to %s: %w", source, target, err)
+	}
+
+	return money.ExchangeRate(rate), nil
 }
